8f6ad1ac36c3ec66eac4d64a7e5a8a92d96babdf
Chris Frank <chris@cfrank.org>
2018-07-16 01:32:42 -0400

    Add a couple helper functions for help license and version printing

--- src/yabi.c
+++ src/yabi.c
@@ -11,6 +11,10 @@
 #define VERSION_MINOR 0
 #define VERSION_PATCH 1
 
+static void print_help(void);
+static void print_license(void);
+static void print_version(void);
+
 int main(int argc, char **argv)
 {
         char opt;
@@ -21,19 +25,19 @@ int main(int argc, char **argv)
                         printf("Parsing file %s...\n", optarg);
                         break;
                 case 'h':
-                        printf("%s\n", "Print the help dialog");
+                        print_help();
                         break;
                 case 'l':
-                        printf("%s\n", "Print the license dialog");
+                        print_license();
                         break;
                 case 'r':
                         printf("%s\n", "Run the REPL shell");
                         break;
                 case 'v':
-                        printf("%s\n", "Print the version");
+                        print_version();
                         break;
                 case '?':
-                        puts("Print the usage info");
+                        print_help();
                         break;
                 default:
                         continue;
@@ -41,4 +45,29 @@ int main(int argc, char **argv)
         }
 
         return EXIT_SUCCESS;
+}
+
+static void print_help(void)
+{
+        print_version();
+        puts("");
+        printf("./yabi\n"
+               "-f <file> - Parse a .torrent file\n"
+               "-h - Print this help information\n"
+               "-l - Print some license information\n"
+               "-r - Run the REPL shell which gives some control over a file\n"
+               "-v - Print the version\n");
+}
+
+static void print_license(void)
+{
+        puts("Yabi is licensed under the BSD 3 Clause license");
+}
+
+static void print_version(void)
+{
+        printf("Yabi - Yet Another Bencoding Implementation %d.%d.%d\n",
+               VERSION_MAJOR,
+               VERSION_MINOR,
+               VERSION_PATCH);
 }
