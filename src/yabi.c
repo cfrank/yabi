8c592677e980969a100c4a38bb98e947d71e58d2
Chris Frank <chris@cfrank.org>
2018-07-15 21:32:45 -0400

    Add getopt code but unistd.h is not working right now

--- src/yabi.c
+++ src/yabi.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "yabi.h"
 #include "yabi_node.h"
 #include "yabi_type.h"
 
-int main(void)
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 1
+
+int main(int argc, char **argv)
 {
-        return EXIT_SUCCESS;
+        int opt;
+
+        while ((opt = getopt(argc, argv, "f:hlv")) != -1) {
+                switch (opt) {
+                case 'f':
+                        printf("Parsing file %c...", optarg);
+                        break;
+                case 'h':
+                        printf("%s", "Print the help dialog");
+                        break;
+                case 'l':
+                        printf("%s", "Print the license dialog");
+                        break;
+                case 'v':
+                        printf("%s", "Print the version");
+                        break;
+                default:
+                        printf("Unknown options %c", opt);
+                }
+        }
 }
